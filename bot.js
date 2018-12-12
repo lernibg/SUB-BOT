18fcfed0d05671456fdc2bc39693bc77f87e268f
lernibg <44763315+lernibg@users.noreply.github.com>
2018-12-12 10:25:31 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+/*
+ * Bot Made By Angshu
+ * ;-;
+ * Nothing xD
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+
+}
